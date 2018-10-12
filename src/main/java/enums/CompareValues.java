fd8287f1b8b9c02bca8532e5a911d5222ed4a51f
oshi192 <yuriykoss@gmail.com>
2018-10-12 10:44:48 +0300

    refactor

--- src/main/java/enums/CompareValues.java
+++ src/main/java/enums/CompareValues.java
@@ -2,7 +2,7 @@ package enums;
 
 public enum CompareValues {
     BIGGER(" ^^^Bigger"), LOWER(" ...lower"), EQUALS(" = equals"), NOT_A_NUMBER(" ?? not a number");
-    String value;
+    public final String value;
 
     CompareValues(String value) {
         this.value = value;
